f8c4d9c2eb76e344abe05a5f63e564bce07a62e2
Waldirio M Pinheiro <waldirio@gmail.com>
2024-09-05 21:25:43 -0700

    updated to work on sat and capsule

--- pulp3_check.sh
+++ pulp3_check.sh
@@ -1,17 +1,18 @@
 #!/bin/bash
 
 
-TASKS=$(pulp task list --state-in waiting --state-in skipped --state-in running --state-in failed --state-in canceled --state-in canceling | python3 -m json.tool | grep -E '(pulp_href|name|started_at|finished_at)')
+TASKS=$(echo "select pulp_id,name,started_at,finished_at,state from core_task where state != 'completed'" | su - postgres -c "psql --expanded pulpcore")
 
 
-if [ "$TASKS" == "" ]; then
+if [ "$TASKS" == "(0 rows)" ]; then
   echo "No tasks at this moment, exiting ..."
   exit 1
 else
   echo "$TASKS"
 fi
 
-
+echo
+echo
 echo -n "Copy/Paste the ID you would like to inspect (e.g. 0191be1a-2f17-7f80-9af6-4b58404b93af): "
 read opc
 
